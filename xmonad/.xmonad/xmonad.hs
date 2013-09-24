4c94e6762c65aa7b9d9742169d6a960012d987ee
Rob Knight <mail@robknight.org.uk>
2013-09-24 13:49:34 +0100

    Tidied up xmobar configuration

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -15,16 +15,9 @@ myManagementHooks = [
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/rknight/.xmobarrc"
 
-    xmonad $ defaultConfig
+    xmonad =<< xmobar defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll myManagementHooks
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
+        , logHook = dynamicLogWithPP $ xmobarPP
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        ]
+        }
