98aa1e87f14199c9ad6b35d824e44d8e386843f8
Rob Knight <mail@robknight.org.uk>
2013-09-24 15:10:06 +0100

    Let's give GridSelect a try

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Layout.Tabbed
+import XMonad.Actions.GridSelect
 
 myManagementHooks :: [ManageHook]
 myManagementHooks = [
@@ -15,9 +16,15 @@ myManagementHooks = [
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/rknight/.xmobarrc"
 
-    xmonad =<< xmobar defaultConfig
+    xmonad =<< xmobar myConfig
+
+myConfig = defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll myManagementHooks
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = dynamicLogWithPP $ xmobarPP
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         }
+        `additionalKeys`
+        [ ((mod4Mask, xK_g), goToSelected myGSConfig) ]
+
+myGSConfig = defaultGSConfig { gs_cellwidth = 160 }
