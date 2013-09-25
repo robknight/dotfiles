aaf20d40a30278fb0d605c4b1747bfc1b39c9bee
Rob Knight <mail@robknight.org.uk>
2013-09-25 09:39:53 +0100

    Experimenting with some GridSelect customization

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -6,6 +7,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Layout.Tabbed
 import XMonad.Actions.GridSelect
+import qualified Data.Map as M
 
 myManagementHooks :: [ManageHook]
 myManagementHooks = [
@@ -27,4 +29,31 @@ myConfig = defaultConfig
         `additionalKeys`
         [ ((mod4Mask, xK_g), goToSelected myGSConfig) ]
 
-myGSConfig = defaultGSConfig { gs_cellwidth = 160 }
+myGSConfig = defaultGSConfig { gs_cellwidth = 250
+                             , gs_navigate = myNavigation
+                             , gs_font = "xft:Bitstream Vera Sans-10"
+                             }
+
+myNavigation :: TwoD a (Maybe a)
+myNavigation = makeXEventhandler $ shadowWithKeymap navKeyMap navDefaultHandler
+ where navKeyMap = M.fromList [
+          ((0,xK_Escape), cancel)
+         ,((0,xK_Return), select)
+         ,((0,xK_g), select)
+         ,((0,xK_slash) , substringSearch myNavigation)
+         ,((0,xK_Left)  , move (-1,0)  >> myNavigation)
+         ,((0,xK_h)     , move (-1,0)  >> myNavigation)
+         ,((0,xK_Right) , move (1,0)   >> myNavigation)
+         ,((0,xK_l)     , move (1,0)   >> myNavigation)
+         ,((0,xK_Down)  , move (0,1)   >> myNavigation)
+         ,((0,xK_j)     , move (0,1)   >> myNavigation)
+         ,((0,xK_k)     , move (0,-1)   >> myNavigation)
+         ,((0,xK_Up)    , move (0,-1)  >> myNavigation)
+         ,((0,xK_y)     , move (-1,-1) >> myNavigation)
+         ,((0,xK_i)     , move (1,-1)  >> myNavigation)
+         ,((0,xK_n)     , move (-1,1)  >> myNavigation)
+         ,((0,xK_m)     , move (1,-1)  >> myNavigation)
+         ,((0,xK_space) , setPos (0,0) >> myNavigation)
+         ]
+       -- The navigation handler ignores unknown key symbols
+       navDefaultHandler = const myNavigation
